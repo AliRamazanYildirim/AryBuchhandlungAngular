a86c7b1b6d0952479f1bf52ed53994911e845bba
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-11-20 17:01:00 +0300

    Fixind Startwerte

--- AryBuchhandlungAPI/AryBuchhandlungAPI/Daten/DatenKontext.cs
+++ AryBuchhandlungAPI/AryBuchhandlungAPI/Daten/DatenKontext.cs
@@ -14,18 +14,21 @@ namespace AryBuchhandlungAPI.Daten
             modelBuilder.Entity<AryBuch>().HasData(
                 new AryBuch
                 {
+                    ID=1,
                     Name = "Aspekte Neu B1",
                     Niveau = "B1",
                     Preis = 29.99m
                 },
                 new AryBuch
                 {
+                    ID=2,
                     Name = "Aspekte Neu B2",
                     Niveau = "B2",
                     Preis = 39.99m
                 },
                 new AryBuch
                 {
+                    ID=3,
                     Name = "Aspekte Neu C1",
                     Niveau = "C1",
                     Preis = 49.99m
