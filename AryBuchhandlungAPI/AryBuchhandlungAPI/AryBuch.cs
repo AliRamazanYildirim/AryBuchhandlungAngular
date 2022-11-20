f79afcec2fc30c15cb1f5edb67e29d2504b7a320
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-11-20 09:45:46 +0300

    AryBuchController erstellen

---
+++ AryBuchhandlungAPI/AryBuchhandlungAPI/AryBuch.cs
@@ -0,0 +1,10 @@
+﻿namespace AryBuchhandlungAPI
+{
+    public class AryBuch
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Niveau { get; set; } = string.Empty;
+        public decimal Preis { get; set; }
+    }
+}
---
+++ AryBuchhandlungAPI/AryBuchhandlungAPI/Controllers/AryBuchController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AryBuchhandlungAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AryBuchController : ControllerBase
+    {
+        public AryBuchController()
+        {
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<AryBuch>>> GeheZurBücher()
+        {
+            return new List<AryBuch>
+            {
+                new AryBuch
+                {
+                    Name="Aspekte Neu B1 ",
+                    Niveau="B1",
+                    Preis=29.99m
+                }
+            };
+        }
+    }
+}
